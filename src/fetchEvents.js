3192eab8532ade923145b76779cb338dc55f779f
Erik Larson <erikmlarson5@gmail.com>
2025-05-22 15:47:46 -0400

    fetch all sets implementation

--- src/fetchEvents.js
+++ src/fetchEvents.js
@@ -1,57 +1,58 @@
 import fs from "fs";
 import path from "path";
-import { config } from 'dotenv';
-config();
 
 const key = process.env.STARTGG_API_KEY;
 
-const tournamentPath = path.join(process.cwd(), 'config', 'tournaments.json'); //TODO change tournaments example to regular file
-const eventIdPath = path.join(process.cwd(), 'data', 'eventIds.json')
-const tournamentData = JSON.parse(fs.readFileSync(tournamentPath, 'utf-8'));
+const tournamentPath = path.join(process.cwd(), "config", "tournaments.json");
+const eventIdPath = path.join(process.cwd(), "data", "eventData.json");
+const tournamentData = JSON.parse(fs.readFileSync(tournamentPath, "utf-8"));
 const allEvents = [];
 
 const query = `query getEventId($slug: String) {
   event(slug: $slug) {
     id
     name
+    tournament {
+      name
+    }
   }
-}`
+}`;
 
 async function fetchId(slug) {
-    const response = await fetch("https://api.start.gg/gql/alpha", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${key}`
-        },
-        body: JSON.stringify({
-            query,
-            variables: { slug }
-        })
-    });
+  const response = await fetch("https://api.start.gg/gql/alpha", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      query,
+      variables: { slug },
+    }),
+  });
 
-    const data = await response.json();
-
-    if (data.data && data.data.event) {
-        return data.data.event;
-    }
-
+  const data = await response.json();
+  if (data.data && data.data.event) {
+    return data.data.event;
+  }
 }
 
-async function parseAndFetch() {
+export async function fetchEvents() {
   for (const tournament of tournamentData) {
     const slug = tournament.substring("https://www.start.gg/".length);
     const event = await fetchId(slug);
     if (event) {
-        allEvents.push({
-            id: event.id,
-            name: event.name,
-            slug: slug
-        })
+      allEvents.push({
+        tournament: event.tournament.name,
+        event: event.name,
+        id: event.id,
+        slug: slug,
+      });
+      console.log(`Fetching event data from ${event.tournament.name}`);
+    } else {
+      console.log(`No event found for slug: ${slug}`);
     }
   }
   fs.writeFileSync(eventIdPath, JSON.stringify(allEvents, null, 2));
-  console.log("All events successfully written to eventIds.json")
+  console.log("All events successfully written to eventIds.json");
 }
-
-parseAndFetch();
